a9b8bdc23064e659307ddb8d09eca20d9aaa520b
Indiwara65 <indiwarananayakkara1@gmail.com>
2025-06-16 07:16:00 +0530

    data classes module added

---
+++ src/data_classes/__init__.py
@@ -0,0 +1,5 @@
+__version__ = "1.0.0"
+
+from .data_classes import User, Login, Session
+
+__all__ = [User, Login, Session]
---
+++ src/data_classes/data_classes.py
@@ -0,0 +1,22 @@
+from enum import Enum
+from pydantic import BaseModel, Field
+
+#Data Models
+class Role(str, Enum):
+    admin       = "admin"
+    jobManager  = "job manager"
+    storeKeeper = "store keeper"  
+    worker      = "worker"
+
+class User(BaseModel):
+    firstname : str = Field(min_length=3, max_length=24)
+    lastname  : str = Field(min_length=3, max_length=24)
+    password  : str = Field(min_length=10)
+    role      : Role
+
+class Login(BaseModel):
+    username : str
+    password : str
+
+class Session(BaseModel):
+    sessionID : int
